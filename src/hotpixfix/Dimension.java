e2a805af9a210aee0ac23cd80d706bf682a82951
Master Chief <extra07@online.de>
2012-06-02 13:31:24 +0200

    added my own dimension

---
+++ src/hotpixfix/Dimension.java
@@ -0,0 +1,37 @@
+package hotpixfix;
+
+/**
+ * This class implements an int Dimension
+ */
+class Dimension {
+
+  /** width of this */
+  private int width;
+
+  /** height of this */
+  private int height;
+
+  /**
+   * Constructs a new Dimension 
+   * out of th given width and height
+   * @param width the given width
+   * @param height the given height
+   */
+  public Dimension(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  /**
+   * Returns the width of this
+   * @return the width of this
+   */
+  public int getWidth() { return width; }
+
+  /**
+   * Returns the height of this
+   * @return the height of this
+   */
+  public int getWidth() { return height; }
+
+}
